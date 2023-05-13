86c4acae1dd1bacfaba77954e13455f2c1a8d69a
Anton Mak <Anton3841@yandex.ru>
2023-05-13 21:19:09 +0300

    addPagesWithRouter

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -2,16 +2,33 @@ import { Axios } from "axios";
 import axios from 'axios';
 export default class PostService {
     static async getAll(limit = 10, page = 1) {
-       
-            const response = await axios.get(
-                "https://jsonplaceholder.typicode.com/posts",{
-                params: {
-                    _limit: limit,
-                    _page: page
-            }
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/posts",{
+                    params: {
+                        _limit: limit,
+                        _page: page
+                    }
              } );
-              return response;
-       
-      
+         return response;    
     }
+
+
+    static async getById(id) {
+       
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/posts/" +id);
+          return response;
+   
+  
+}
+    static async getCommentByPostId(id) {
+       
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+          return response;
+   
+  
+}
+
+
 }
